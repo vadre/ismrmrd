b84323d61d6b6ec4100cbaf265380a06b2a9250c
vadre <vlad.dreyvitser@nih.gov>
2016-03-22 18:48:42 -0400

    Initial commit

---
+++ examples/protocol/icpReceivedData.cpp
@@ -0,0 +1,170 @@
+#include "ismrmrd/ismrmrd.h"
+#include "icpReceivedData.h"
+
+#include <sstream>
+#include <stdexcept>
+#include <iostream>
+#include <algorithm>
+
+#include <string.h>
+
+
+namespace ICPRECEIVEDDATA
+{
+  /*****************************************************************************
+   ****************************************************************************/
+  icpStream::icpStream
+  (
+    ISMRMRD::EntityType  ent = ISMRMRD::ISMRMRD_ERROR,
+    ISMRMRD::StorageType sto = ISMRMRD::ISMRMRD_CHAR,
+    ISMRMRD::CommandType cmd = ISMRMRD::ISMRMRD_COMMAND_NO_COMMAND,
+    ISMRMRD::ErrorType   err = ISMRMRD::ISMRMRD_ERROR_NO_ERROR,
+    bool                 complete = false) :
+    entity_type  (ent),
+    storage_type (sto),
+    this->complete (complete)
+  {
+    if (cmd != ISMRMRD::ISMRMRD_COMMAND_NO_COMMAND)
+    {
+      commands.push (cmd);
+    }
+
+    if (err != ISMRMRD::ISMRMRD_ERROR_NO_ERROR)
+    {
+      errors.push (err);
+    }
+  }
+
+  /*****************************************************************************
+   ****************************************************************************/
+  icpStream& icpStream::operator= (const icpStream& o)
+  {
+    if (this == &o) return *this;
+
+    this->commands      = o.commands;
+    this->errors        = o.errors;
+    this->entity_type   = o.entity_type;
+    this->strorage_type = o.storage_type;
+    this->description   = o.description;
+    this->data          = o.data;
+    this->complete      = o.complete;
+
+    return *this;
+  }
+
+  /*****************************************************************************
+   ****************************************************************************/
+  ReceivedData::RecievedData ()
+  {
+    memset (sender_name, 0, ISMRMRD::Max_Client_Name_Length);
+    session_timestamp = 0;
+  }
+
+  /*****************************************************************************
+   ****************************************************************************/
+  ReceivedData::RecievedData (char* name, uint64_t timestamp)
+  {
+    ReceivedData::setSessionTimestamp (timestamp);
+    ReceivedData::setSenderName (name);
+  }
+
+  /*****************************************************************************
+   ****************************************************************************/
+  void ReceivedData::setSenderName (char* name)
+  {
+    memset (sender_name, 0, ISMRMRD::Max_Client_Name_Length);
+    if (name)
+    {
+      size_t size = strlen (name);
+      size = (size > ISMRMRD::Max_Client_Name_Length) ?
+              ISMRMRD::Max_Client_Name_Length : size;
+      strncpy (sender_name, name, size);
+    }
+  }
+
+  /*****************************************************************************
+   ****************************************************************************/
+  char* ReceivedData::getSenderName ()
+  {
+    memset (sender_name, 0, ISMRMRD::Max_Client_Name_Length);
+    if (name)
+    {
+      size_t size = strlen (name);
+      size = (size > ISMRMRD::Max_Client_Name_Length) ?
+              ISMRMRD::Max_Client_Name_Length : size;
+      strncpy (sender_name, name, size);
+    }
+  }
+
+  /*****************************************************************************
+   ****************************************************************************/
+  void ReceivedData::setSessionTimestamp (uint64_t timestamp)
+  {
+    session_timestamp = timestamp;
+  }
+
+  /*****************************************************************************
+   ****************************************************************************/
+  uint64_t ReceivedData::getSessionTimestamp ()
+  {
+    return session_timestamp;
+  }
+
+  /*****************************************************************************
+   ****************************************************************************/
+  void ReceivedData::checkStreamExist
+  (
+    uint32_t                stream,
+    ISMRMRD::EntityType     ent,
+    ISMRMRD::StorageType    stor
+  )
+  {
+    if (streams.find (stream) == streams.end())
+    {
+      streams (stream) = icpStream tmp (ent, stor);
+    }
+    else
+    {
+      if ( streams.at (stream).entity_type != ent ||
+           streams.at (stream).storage_type != stor )
+      {
+        // Throw an error - all messages of a stream must have the same
+        // entity type and storage type
+        std::cout << "Entity and/or Storage types for stream " << stream <<
+                     " differ" << std::endl;
+      }
+    }
+  }
+
+  /*****************************************************************************
+   ****************************************************************************/
+  void ReceivedData::addCommand (ISMRMRD::EntityHeader  hdr,
+                                 ISMRMRD::Command       cmd)
+  {
+    checkStreamExist (hdr.stream, hdr.entity_type, hdr.storage_type);
+
+    streams.at (hdr.stream).commands.push (cmd.command_type);
+
+    if (cmd.error_type != ISMRMRD::ISMRMRD_ERROR_NO_ERROR)
+    {
+      streams.at (hdr.stream).errors.push (cmd.error_type);
+    }
+
+    // Evaluate the command/error to see if an action is immediately needed.
+  }
+
+  /*****************************************************************************
+   ****************************************************************************/
+  void ReceivedData::addToStream
+  (
+    uint32_t           stream,
+    ISMRMRD::EntityType,
+                             ISMRMRD::StorageType  stor,
+    uint32_t           size,
+    std::vector<unsigned char> data
+  )
+  {
+  }
+
+
+} /* namespace ICPRECEIVEDDATA */
---
+++ examples/protocol/icpReceivedData.h
@@ -0,0 +1,84 @@
+/*******************************************************************************
+ * Heterogeneous collection of entities received from network connection during
+ * communication session via ISMRMRD Communication Protocol (version 2.x).
+ *
+ ******************************************************************************/
+
+/**
+ * @file icpReceivedData.h
+ */
+
+#pragma once
+#ifndef ICPRECEIVEDDATA_H
+#define ICPRECEIVEDDATA_H
+
+#include <complex>
+#include <vector>
+#include "ismrmrd/ismrmrd.h"
+
+namespace ICPRECEIVEDDATA
+{
+
+struct icpStream
+{
+  icpStream (ISMRMRD::EntityHeader,
+             ISMRMRD::CommandType,
+             ISMRMRD::ErrorType,
+             bool complete);
+
+  icpStream& operator = (const icpStream &other);
+
+
+  // Potentially multiple commands and/or errors may be
+  // associated with a stream.
+  std::queue<ISMRMRD::CommandType>         commands;
+  std::queue<ISMRMRD::ErrorType>           errors;
+  ISMRMRD::EntityHeader                    entity_header;
+  std::queue<std::vector<unsigned char> >  data;
+  // Every new stream is created with the complete flag
+  // initialized to false. Determination whether a parti-
+  // cular stream is made later using the data-specific
+  // criteria. In some cases a strem can be determined
+  // complete when the STOP/DONE command is received.
+  bool                                     complete;
+};
+
+
+class ReceivedData
+{
+public:
+
+  RecievedData ();
+  RecievedData (char* name, uint64_t timestamp = 0);
+
+  void  setSenderName (char* name);
+  char* getSenderName();
+
+  void     setSessionTimestamp (uint64_t timestamp);
+  uint64_t getSessionTimestamp ();
+
+  void addCommand (ISMRMRD::EntityHeader  header,
+                   ISMRMRD::Command       command);
+
+  void addToStream (ISMRMRD::EntityHeader hdr);
+
+  bool getCommand  (ISMRMRD::Command& command,
+                    uint32_t&         stream);
+
+protected:
+
+  uint32_t checkStreamExist (uint32_t stream,
+                             ISMRMRD::EntityType,
+                             ISMRMRD::StorageType);
+
+  char      sender_name[ISMRMRD::Max_Client_Name_Length];
+  uint64_t  session_timestamp;
+
+  std::map<uint32_t, icpStream>  streams;
+
+  
+}; /* class ReceivedData */
+
+} /* namespace ICPRECEIVEDDATA */
+
+#endif /* ICPRECEIVEDDATA_H */
